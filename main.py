ab42ae0eac26ddafdd7eb467a74c9c5e2dfb2e9a
Huy-Ngo <duchuy29092000@gmail.com>
2020-02-08 17:52:23 +0700

    Add basic commands

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import discord
 from discord.ext import commands
+import random
 from json import load
 
-bot = commands.Bot(command_prefix='>')
+description = """An example bot to showcase the discord.ext.commands extension
+module.
+There are a number of utility commands being showcased here."""
+
+bot = commands.Bot(command_prefix='dd ')
 
 # Read secrets
 with open('./secret/secret.json', 'r') as f:
@@ -10,8 +15,24 @@ with open('./secret/secret.json', 'r') as f:
     token = secret['token']
 
 
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def introduce(ctx):
+    """Introduces the bot"""
+    await ctx.send('I will apply Google\'s algorithm *DeepDream* to your image')
+
+
+@bot.command()
+async def dream(ctx, image_link):
+    """Apply DeepDream to your image"""
+    ctx.send('Not implemented yet')
+
 
 bot.run(token)
